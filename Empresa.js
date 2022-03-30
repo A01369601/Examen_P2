a628ccc75fa60ef9f299b2f62d2587ed2304a61b
Jonathan <99694616+A01369601@users.noreply.github.com>
2022-03-29 20:46:25 -0600

    Add files via upload

---
+++ Empresa.js
@@ -0,0 +1,48 @@
+function vocales(){
+	voc="àèìòùâêîôû";
+	letra="?";
+	for(i=1;i<ap.length;i++){
+		letra=ap.charAt(i);
+		if (voc.includes(letra)) return(letra);
+	}
+	return(letra);
+}
+
+function ejecuta(){
+	nom=document.TEC.nom.value;
+    document.TEC.nom.style.background="blue";
+    if (nom.length==0){
+		document.TEC.nom.style.background="red";
+		alert("Error, se debe indicar el Nombre ...");
+	}
+	ap=document.TEC.ap.value;
+    document.TEC.ap.style.background="blue";
+    if (ap.length==0){
+		document.TEC.ap.style.background="red";
+		alert("Error, se debe indicar el Apellido paterno ...");
+	}
+	am=document.TEC.am.value;
+    document.TEC.am.style.background="blue";
+    if (am.length==0){
+		document.TEC.am.style.background="red";
+		alert("Error, se debe indicar el Apellido materno ...");
+	}
+	fec=document.TEC.fec.value;
+	nom=nom.toUpperCase();
+	ap=ap.toUpperCase();
+	am=am.toUpperCase();
+	anio=fec.substr(2,2);
+	mes=fec.substr(5,2);
+	dia=fec.substr(8,2);
+	correo=nom.charAt(0).toLowerCase()+ap.toLowerCase()+am.charAt(0).toLowerCase()+"@empresa.com.mx.aa";
+    correo=correo.replaceAll("ñ","n");
+    correo=correo.replaceAll(" ","_");
+    correo=correo.replaceAll("á","a");
+    correo=correo.replaceAll("é","e");
+    correo=correo.replaceAll("í","i");
+    correo=correo.replaceAll("ó","o");
+    correo=correo.replaceAll("ú","u");
+    document.TEC.salida.value=correo;
+    contra=dia+mes+anio;
+    document.TEC.contra.value=contra;
+}
